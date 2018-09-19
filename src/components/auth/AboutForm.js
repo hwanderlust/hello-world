506ade981ae4387dfe0e1ce2f72426381b66ca1e
Paul <lpault29@gmail.com>
2018-09-19 13:21:52 -0400

    auth country lang inputs to select

--- src/components/auth/AboutForm.js
+++ src/components/auth/AboutForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { countries, googleLanguages } from '../../constants';
 
 class AboutForm extends React.Component {
 
@@ -8,24 +9,38 @@ class AboutForm extends React.Component {
 
   render() {
     const { handleSubmit, handleChange, location, age, nationality, languages } = this.props
+    const renderCountries = () => {
+      return countries.map(country => <option key={country}>{country}</option>)
+    }
+    const renderLanguages = () => {
+      return googleLanguages.map(lang => <option key={lang.code}>{lang.name}</option>)
+    }
 
-  return (
-    <form onSubmit={(e) => handleSubmit(e)} id='aboutForm' className='auth-children'>
-      <label htmlFor='location'>Where you at?</label><br/>
-      <input type='text' name='location' className='inputs' onChange={handleChange} value={location} autoFocus="true" ref={c => (this.inputFocus = c)} />
-      <br />
-      <label htmlFor='age'>How wise are you? (age)</label>
-      <input type='number' name='age' className='inputs' onChange={handleChange} value={age} />
-      <br />
-      <label htmlFor='nationality'>Where's your passport from?</label>
-      <input type='text' name='nationality' className='inputs' onChange={handleChange} value={nationality} />
-      <br />
-      <label htmlFor='languages'>Which languages do you speak?</label>
-      <input type='text' name='languages' className='inputs' onChange={handleChange} value={languages} />
-      <br />
-      <input type='submit' className='' />
-    </form>
-  )}
+    return (
+      <form onSubmit={(e) => handleSubmit(e)} id='aboutForm' className='auth-children'>
+        <label htmlFor='location'>Where you at?</label><br/>
+        <input type='text' name='location' className='inputs' onChange={handleChange} value={location} autoFocus="true" ref={c => (this.inputFocus = c)} />
+        <br />
+        <label htmlFor='age'>How wise are you? (age)</label>
+        <input type='number' name='age' className='inputs' onChange={handleChange} value={age} />
+        <br />
+        <label htmlFor='nationality'>Where's your passport from?</label>
+        <select>
+          <option key='default' id='default' disabled selected>Choose one</option>
+          { renderCountries() }
+        </select>
+        {/* <input type='text' name='nationality' className='inputs' onChange={handleChange} value={nationality} /> */}
+        <br />
+        <label htmlFor='languages'>Which languages do you speak?</label>
+        <select multiple>
+          <option key='default' id='default' disabled selected>Choose one</option>
+          { renderLanguages() }
+        </select>
+        {/* <input type='text' name='languages' className='inputs' onChange={handleChange} value={languages} /> */}
+        <br />
+        <input type='submit' className='' />
+      </form>
+    )}
 };
 
 export default AboutForm;
